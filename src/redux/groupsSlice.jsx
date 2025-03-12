20e2c9f84ddf5479730d85a8f25bf061d89e7866
Vuong Quoc Anh <vuongquocanh300402@gmail.com>
2025-03-12 09:15:42 +0700

    link to group form and modify group form

--- src/redux/groupsSlice.jsx
+++ src/redux/groupsSlice.jsx
@@ -1,12 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {
-    list: ["Nhóm A", "Nhóm B", "Nhóm C"], // Dữ liệu mặc định
-};
-
 const groupsSlice = createSlice({
     name: "groups",
-    initialState,
+    initialState: {
+        list: [],
+    },
     reducers: {
         addGroup: (state, action) => {
             state.list.push(action.payload);
